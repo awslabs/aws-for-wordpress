d4de38422e4d99093ce95a56f6ee0afdd6b81f1c
stachlew <stachlew@amazon.pl>
2018-09-14 20:34:16 +0200

    cleaning

--- vendor/aws/aws-sdk-php/src/data/endpoints_prefix_history.json.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This file was auto-generated from sdk-root/src/data/endpoints_prefix_history.json
-return [ 'prefix-groups' => [ 'api.sagemaker' => [ 'sagemaker', ], 'sagemaker' => [ 'api.sagemaker', ], ],];
